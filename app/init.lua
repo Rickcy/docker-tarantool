3b8788d9908d6bc4cd894d93bae4815883d56455
rickcy <kuden.and.ko@gmail.com>
2020-03-01 23:54:35 +0300

    refactoring..

--- app/init.lua
+++ app/init.lua
@@ -1,11 +1,25 @@
-#!/usr/bin/env tarantool
-box.cfg{}
+#!   /usr/bin/env tarantool
+box.cfg {}
 pg = require('pg')
+os = require('os');
+host = os.getenv('POSTGRES_HOST')
+port = os.getenv('POSTGRES_PORT')
+username = os.getenv('POSTGRES_USERNAME')
+password = os.getenv('POSTGRES_PASSWORD')
+db = os.getenv('POSTGRES_DB')
+
 conn = pg.connect({
-    host = '192.168.1.203',
-    port = 5432,
-    user = 'test',
-    password = 'test',
-    db = 'test'
+    host = host,
+    port = port,
+    user = username,
+    password = password,
+    db = db
 })
-conn:ping()
+
+function select(query)
+    local test = conn:execute(query)
+    local rows = {}
+    for i, card in pairs(test) do
+        row = row .. card.s2 .. ' '
+    end
+end
